119d27da4231ebe694e4f1f94c38953982b89db9
andrewrmobberley <andrewrmobberley@gmail.com>
2017-03-21 14:27:51 +0000

    Update InputFileCreator.py

--- InputFileCreator.py
+++ InputFileCreator.py
@@ -9,11 +9,12 @@ cwd = os.getcwd()
 listofposlist =[]
 labellist =[]
 
+#takes data from files in a directory
 for file in glob.glob(str(cwd)+'/PlanetsUniq/*.txt'):
     linepos = 0
     with open(file, 'r') as file2:
         filedata = file2.read()
-
+#finds the mass data in the file
     mass = filedata.find('kg')
     massend = filedata[mass:].find('$')
     mass1 = float(filedata[mass:massend+mass].translate(None, 'kg )~=-+'))
@@ -31,18 +32,19 @@ for file in glob.glob(str(cwd)+'/PlanetsUniq/*.txt'):
         massorder = float(filedata[massorder3+len('Mass Pluto (10^'):massorder3+len('Mass Pluto (10^')+2])
 
     mass2 = mass1*10.0**float(massorder)
-    
+    #finds the position data in the files
     pos = filedata.find('$$SOE')
     pos = filedata[pos+6: pos + filedata[pos+6:].find('\n')]
     poslist = pos.split(',')
     del poslist[0], poslist[0],poslist[-1],poslist[-1],poslist[-1]
-
+#adds the found data to lists
     poslist.insert(0, mass2)
     listofposlist.append(poslist)
+    #finds the name of the body and adds it to a list
     name = str(file)
     name = name[name.find("PlanetsUniq/")+12:name.find(".txt")]
     labellist.append(name)
-
+#puts the data from the files into arrays before collecting the data with the correct labels into a final array
 bodies = np.array(listofposlist)
 labels = np.array(labellist)
 bodiesfinal = np.insert(bodies, 1,labels,axis=1)
